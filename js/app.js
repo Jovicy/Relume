5b62389646a81795dc370f4b85df6b97cbabb387
Bamidele <bamsjovicy@gmail.com>
2023-03-23 23:44:47 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// Carousel for the testimonial
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("testimonials__cont");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "flex";
+}
