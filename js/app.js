e015b76b362c36497575515a8eec582427a86eee
Bamidele <bamsjovicy@gmail.com>
2023-03-24 15:17:35 +0100

    working on mobile responsiveness

--- js/app.js
+++ js/app.js
@@ -13,10 +13,37 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("testimonials__cont");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "flex";
-}
+  slides[slideIndex - 1].style.display = "flex";
+};
+
+// ToptolBar toggle
+const toggleBtn = document.querySelector(".btn__close");
+const links = document.querySelector(".toptolbar");
+
+toggleBtn.addEventListener("click", function () {
+  links.classList.toggle("hide");
+});
+
+
+// ToggleBtn
+const toggler = document.querySelector('.btn__menu'),
+  navListContainer = document.querySelector('.nav__container');
+
+/*when toggler button is clicked*/
+toggler.addEventListener(
+  "click",
+  () => {
+    //make nav visible
+    navListContainer.classList.toggle('nav-active');
+  },
+  true
+);
